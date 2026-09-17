64c6a157ee319ec6e3268aa702f82a423ea48aef
sangeethkvijayan <sangeethkvijayan9539@gmail.com>
2026-09-17 14:36:51 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "ec2" {
+ 
+  ami           = var.ami_id
+ 
+  instance_type = var.instance_type
+ 
+  tags = {
+    Name        = var.instance_name
+    Project     = "TBDP-BI DevOps"
+    Environment = "DEV"
+  }
+}
