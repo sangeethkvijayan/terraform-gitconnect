f5993b834631067fdabcbfff842d3d8de0dccaa4
sangeethkvijayan <sangeethkvijayan9539@gmail.com>
2026-09-18 15:13:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,4 +3,11 @@ resource "aws_s3_bucket" "tbdp-ing" {
   bucket = "sangeeth-demo-bucket"
 
 }
- 
+
+module "my_server" {
+  source = "./modules/ec2_instance"
+
+  # Pass input variables required by the module
+  instance_type = "t3.medium"
+  environment   = "production"
+}
